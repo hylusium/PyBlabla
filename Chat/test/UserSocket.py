9fff5ef4fe99e478cee0ed841d587c9f085f4ad2
hylusium <hugo.drevet@hotmail.fr>
2024-09-30 20:21:06 +0200

    resolve websocket server connexion

--- Chat/test/UserSocket.py
+++ Chat/test/UserSocket.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def hello():
-    uri = "ws://192.168.1.34:8765"
+    uri = "ws://90.5.227.209:8765"
     async with websockets.connect(uri) as websocket:
         await websocket.send("Bonjour, julio!")
         response = await websocket.recv()
