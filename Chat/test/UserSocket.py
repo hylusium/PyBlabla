0510153db706191ca880c8f7aa44fc563c9599c1
hylusium <hugo.drevet@hotmail.fr>
2024-09-30 20:40:21 +0200

    updating response from the server

--- Chat/test/UserSocket.py
+++ Chat/test/UserSocket.py
@@ -1,12 +1,16 @@
 import asyncio
 import websockets
 
-async def hello():
-    uri = "ws://90.5.227.209:8765"
-    async with websockets.connect(uri) as websocket:
-        await websocket.send("Bonjour, julio!")
-        response = await websocket.recv()
-        print(f"Réponse du serveur: {response}")
+async def send_message():
+    uri = "ws://90.5.227.209:8765"    async with websockets.connect(uri) as websocket:
+        # Envoyer un message au serveur
+        message = "Salut, serveur !"
+        await websocket.send(message)
+        print(f"Message envoyé : {message}")
 
-# Lancer le client WebSocket
-asyncio.run(hello())
+        # Recevoir la réponse du serveur
+        response = await websocket.recv()
+        print(f"Réponse du serveur : {response}")
+
+# Lancer le client
+asyncio.get_event_loop().run_until_complete(send_message())
