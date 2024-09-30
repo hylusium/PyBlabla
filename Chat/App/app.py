bb8b71ce8dd6593fdf7f880d49ed39a6ed0714ef
Julio-Branco <j.costabranco@ecoles-epsi.net>
2024-09-30 21:01:38 +0200

    fix

--- Chat/App/app.py
+++ Chat/App/app.py
@@ -1,8 +1,6 @@
 import customtkinter as ctk
 from tkinter import messagebox
 from tkinter import simpledialog
-import asyncio
-import websockets
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
--- Chat/test/UserSocket.py
+++ Chat/test/UserSocket.py
@@ -5,16 +5,10 @@ import websockets
 async def send_message():
     uri = "ws://90.5.227.209:8765"
     async with websockets.connect(uri) as websocket:
-<<<<<<< HEAD
-        await websocket.send("Bonjour, Hugo, je te bouffe le cul!")
-        response = await websocket.recv()
-        print(f"Réponse du serveur: {response}")
-=======
         # Envoyer un message au serveur
         message = input("entrer votre message a envoyer : ")
         await websocket.send(message)
         print(f"Message envoyé : {message}")
->>>>>>> f6ef836b2026c39e36ebce10c81a0daa2264e003
 
         # Recevoir la réponse du serveur
         response = await websocket.recv()
